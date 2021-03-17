72615f1bdf2e6e567066825c8b6725610c12aef1
arkham <nits.slimshady@gmail.com>
2021-03-17 10:21:36 +0530

    log checks

--- src/main/java/com/arkham/impl/Log.java
+++ src/main/java/com/arkham/impl/Log.java
@@ -7,9 +7,25 @@ import org.apache.logging.log4j.Logger;
 public class Log {
 
     private static Logger getLogger(){
-        String className = Thread.currentThread().getStackTrace()[1].getClassName();
+        String className = Thread.currentThread().getStackTrace()[3].getClassName();
         Logger logger = LogManager.getLogger(className);
         return logger;
+
+
+
+
+
+/*        for(StackTraceElement ste : Thread.currentThread().getStackTrace()) {
+            System.out.println(ste);
+        }*/
+
+/*
+*
+* java.lang.Thread.getStackTrace(Thread.java:1559)
+com.arkham.impl.Log.getLogger(Log.java:11)
+com.arkham.impl.Log.logInfo(Log.java:19)
+com.arkham.impl.LogCheck.main(LogCheck.java:6)
+* */
     }
 
     public static void logInfo(String info){
--- src/main/java/com/arkham/impl/LogCheck.java
+++ src/main/java/com/arkham/impl/LogCheck.java
@@ -3,6 +3,6 @@ package com.arkham.impl;
 public class LogCheck {
 
     public static void main(String[] args) {
-        Log.logInfo("Hi I am logging this via abstraction. The class name must be App");
+        Log.logInfo("Hi I am logging this via abstraction. The class name must be LogCheck");
     }
 }
